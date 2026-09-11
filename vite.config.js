38da569125be1e9a5e10ad09c310e744afbe0a54
Mauro <mauro@example.com>
2026-09-11 17:26:01 +0200

    Fix: Bundle react-native-web instead of externalizing to resolve browser module errors

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-native-web',
-        'react-native-web/dist/apis/StyleSheet/registry'
-      ]
-    }
-  }
 })
