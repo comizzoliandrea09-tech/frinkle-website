df7b40646a8ec3cc878f9f434de746a76d989942
Mauro <mauro@example.com>
2026-09-11 19:22:33 +0200

    Fix: Make StyleRegistry a callable function with a resolve method

--- src/empty-module.js
+++ src/empty-module.js
@@ -1,5 +1,6 @@
-export default function() {
-  return {
-    resolve: (style) => style
-  };
+export default function StyleRegistry() {
+  return {};
 }
+StyleRegistry.resolve = function(style) {
+  return style;
+};
